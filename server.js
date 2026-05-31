868cb051fd672a9efec4f61a3b3f14212c9602e4
Truman <trumanjfchan@gmail.com>
2026-05-30 18:10:24 -0700

    feat: public key endpoint

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get(
+  '/.well-known/appspecific/com.tesla.3p.public-key.pem',
+  (req, res) => {
+    res.type('application/x-pem-file');
+    res.send(process.env.TESLA_PUBLIC_KEY);
+  }
+);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
